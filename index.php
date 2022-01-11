528786859bf24b56ea16ca4fc8646b362d3c4a37
Hardy <hardykathurima@gmail.com>
2022-01-11 20:50:38 +0300

    added m-pesa integration

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
-
 $mobile = $amount = $success = "";
-$mobileRegex = "/^[2][5][4]+[\d]{9,}$/";
-$amountRegex = "/^([1-9][0-9]?|100)$/";
-
-
-if (isset($_POST["submit"])) {
-    if (empty($_POST['mobile']) || empty($_POST["amount"])) {
-        $mobile = $_POST["mobile"];
-        $amount = $_POST["amount"];
-    } else {
-        $mobile = $_POST["mobile"];
-        $amount = $_POST["amount"];
-        if (preg_match($mobileRegex, $_POST['mobile'])) {
-            $mobile = $_POST["mobile"];
-        }
-        if (preg_match($amountRegex, $_POST['amount'])) {
-            $amount = $_POST["amount"];
-        }
-
-        $success = "Payment of sh $amount.00 to M-PESA was successful";
-    }
-}
-
 ?>
 
 
@@ -38,7 +15,7 @@ if (isset($_POST["submit"])) {
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Lipa na m-pesa</a>
+        <a class="navbar-brand" href="#">Hardy Inc</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -52,34 +29,27 @@ if (isset($_POST["submit"])) {
         </div>
     </nav>
     <div class="container">
-
-
-
-        <div class="d-flex justify-content-center my-5 ">
-
-            <div class="card w-75 ">
-                <div class="card-header">
-                    <h3 class="text-center mt-3">Lipa na M-PESA stk push</h3>
-                </div>
-                <div class="card-body">
-                    <form method="POST">
-                        <label for="mobile">Mobile Number</label>
-                        <div class="form-group">
-                            <input type="text" name="mobile" id="mobile" class="form-control" placeholder="enter mobile" value="<?php echo $mobile ?>" required pattern="^[2][5][4]+[\d]{9,}$" title="25474356798">
-                        </div>
-                        <label for=amount">Amount</label>
-                        <div class="form-group">
-                            <input type="text" name="amount" id=amount" class="form-control" placeholder="enter amount" value="<?php echo $amount ?>" required pattern="^([1-9][0-9]?|100)$" title="sh 1.00 - sh 99.00">
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" name="submit" class="btn btn-success" value="make payment">
-                        </div>
-                    </form>
-                    <?php if ($success) : ?>
-                        <div class="alert alert-success text-center alert-dismissible "> <?php echo $success ?>
-                            <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        </div>
-                    <?php endif; ?>
+        <div class="row justify-content-center my-4  ">
+            <div class="col-md-8 col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="text-center">Lipa na M-PESA</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="process.php" method="POST">
+                            <label for="mobile">Mobile Number</label>
+                            <div class="form-group">
+                                <input type="text" name="mobile" id="mobile" class="form-control" placeholder="enter mobile" value="<?php echo $mobile ?>" required pattern="^[2][5][4]+[\d]{9,}$" title="25474356798">
+                            </div>
+                            <label for=amount">Amount</label>
+                            <div class="form-group">
+                                <input type="text" name="amount" id=amount" class="form-control" placeholder="enter amount" value="<?php echo $amount ?>" required pattern="^([1-9][0-9]?|100)$" title="sh 1.00 - sh 100.00">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-success" value="make payment">
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ process.php
@@ -0,0 +1,150 @@
+<?php
+
+$mobile = $amount = $success = "";
+$mobileRegex = "/^[2][5][4]+[\d]{9,}$/";
+$amountRegex = "/^([1-9][0-9]?|100)$/";
+
+
+if (isset($_POST["submit"])) {
+    if (empty($_POST['mobile']) || empty($_POST["amount"])) {
+        $mobile = $_POST["mobile"];
+        $amount = $_POST["amount"];
+    } else {
+        $mobile = $_POST["mobile"];
+        $amount = $_POST["amount"];
+        if (preg_match($mobileRegex, $_POST['mobile'])) {
+            $mobile = $_POST["mobile"];
+        }
+        if (preg_match($amountRegex, $_POST['amount'])) {
+            $amount = $_POST["amount"];
+        }
+
+        date_default_timezone_set('Africa/Nairobi');
+
+        # access token
+        $consumerKey = 'PAFQk3PsEJApwhZEt3OdPPdppbmIr7cb'; //Fill with your app Consumer Key
+        $consumerSecret = '534idfFAjtkAGCVR'; // Fill with your app Secret
+
+        # define the variales
+        # provide the following details, this part is found on your test credentials on the developer account
+        $BusinessShortCode = '174379';
+        $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+
+        /*
+    This are your info, for
+    $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
+    $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
+    TransactionDesc can be anything, probably a better description of or the transaction
+    $Amount this is the total invoiced amount, Any amount here will be 
+    actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
+    for developer/test accounts, this money will be reversed automatically by midnight.
+  */
+
+        $PartyA = $_POST['mobile']; // This is your phone number, 
+        $AccountReference = '2255';
+        $TransactionDesc = 'Test Payment';
+        $Amount = $_POST['amount'];;
+
+        # Get the timestamp, format YYYYmmddhms -> 20181004151020
+        $Timestamp = date('YmdHis');
+
+        # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+        $Password = base64_encode($BusinessShortCode . $Passkey . $Timestamp);
+
+        # header for access token
+        $headers = ['Content-Type:application/json; charset=utf8'];
+
+        # M-PESA endpoint urls
+        $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+        $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+        # callback url
+        $CallBackURL = 'https://lipa-na-m-pesa-hardy.herokuapp.com//callback_url.php';
+
+        $curl = curl_init($access_token_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_HEADER, FALSE);
+        curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+        $result = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $result = json_decode($result);
+        $access_token = $result->access_token;
+        curl_close($curl);
+
+        # header for stk push
+        $stkheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
+
+        # initiating the transaction
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $initiate_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+        $curl_post_data = array(
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => $BusinessShortCode,
+            'Password' => $Password,
+            'Timestamp' => $Timestamp,
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => $Amount,
+            'PartyA' => $PartyA,
+            'PartyB' => $BusinessShortCode,
+            'PhoneNumber' => $PartyA,
+            'CallBackURL' => $CallBackURL,
+            'AccountReference' => $AccountReference,
+            'TransactionDesc' => $TransactionDesc
+        );
+
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        print_r($curl_response);
+
+        echo $curl_response;
+
+
+        $success = "Payment of sh $amount.00 to M-PESA was successful";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Process payment</title>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center my-4">
+            <div class="col-md-8">
+                <div class="payment">
+                    <div class="alert alert-success text-center alert-dismissible "> <?php echo $success ?>
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    </div>
+                    <h3 class="text-center"><?php echo $success ?></h3>
+                    <p class="text-center">Thankyou for shopping with us</p>
+                    <a href="index.php">Go Back to shopping</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script src="public/jquery-3.4.1.min.js"></script>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $(".alert").delay(3000).slideDown(200, function() {
+                $(this).alert('close');
+            });
+        });
+    </script>
+</body>
+
+</html>
